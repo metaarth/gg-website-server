8a483af813b2f377d14cb93360b36b3dd7e391b7
metaarth <metaarth@gmail.com>
2025-12-18 13:57:05 +0530

    Add initial server setup with Express, dotenv, and nodemon
    - Created package.json to manage dependencies and scripts.
    - Added index.js as the main server file with a basic Express setup.
    - Introduced .gitignore to exclude environment files, logs, and build outputs.
    - Generated package-lock.json for dependency management.

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.get('/', (req,res)=>{
+    res.send('hello')
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
